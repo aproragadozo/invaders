579c876d29a625b38984c5ef7656ffe071a361c2
Balázs Bujna <bujnab@barion.dev>
2024-05-03 09:44:41 +0200

    switched from event queue to key_pressed, started collision detection

--- main.py
+++ main.py
@@ -10,15 +10,15 @@ screen = pygame.display.set_mode((800, 600))
 
 # title and icon
 pygame.display.set_caption("Space Invaders!")
-icon = pygame.image.load("ufo.png")
+icon = pygame.image.load("ufo.png").convert_alpha()
 pygame.display.set_icon(icon)
 
 # background
-background = pygame.image.load("background.jpg")
+background = pygame.image.load("background.jpg").convert_alpha()
 background = pygame.transform.scale(background, (800, 600))
 
 # player
-playerImg = pygame.image.load("spaceship.png")
+playerImg = pygame.image.load("spaceship.png").convert_alpha()
 playerImg = pygame.transform.scale(playerImg, (64, 64))
 playerX = 370
 playerY = 520
@@ -28,15 +28,16 @@ def player(x, y):
     screen.blit(playerImg, (x, y))
 
 # bullet
-bullet = pygame.image.load("bullet.png")
+bullet = pygame.image.load("bullet.png").convert_alpha()
 bullet = pygame.transform.scale(bullet, (20, 20))
 bullet = pygame.transform.rotate(bullet, 90)
+bulletX = 0
 bulletY = playerY - 30
 bulletY_change = 1
 bullet_state = "ready"
 
 # enemy
-enemyImg = pygame.image.load("enemy.png")
+enemyImg = pygame.image.load("enemy.png").convert_alpha()
 # enemyImg = pygame.transform.scale(enemyImg, (64, 64))
 enemyX = random.randint(0, 800)
 enemyY = random.randint(50, 150)
@@ -62,25 +63,29 @@ while running:
     screen.blit(background, (0, 0))
 
 
-    # spaceship movement, bullet firing
+    # being able to quit the game
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYDOWN:
-            match event.key:
-                case pygame.K_LEFT:
-                    playerX_change -= 0.2
-                case pygame.K_RIGHT:
-                    playerX_change += 0.2
-                case pygame.K_SPACE:
-                    if bullet_state is "ready":
-                        bulletX = playerX
-                        fire_bullet(bulletX, bulletY)
         if event.type == pygame.KEYUP:
             playerX_change = 0
 
-   
+# spaceship movement, bullet firing
+
+    is_key_pressed = pygame.key.get_pressed()
+
+    if is_key_pressed[pygame.K_LEFT]:
+        playerX_change -= 0.005
+    if is_key_pressed[pygame.K_RIGHT]:
+        playerX_change += 0.005
+    if is_key_pressed[pygame.K_SPACE]:
+        if bullet_state is "ready":
+                        bulletX = playerX
+                        fire_bullet(bulletX, bulletY)
+                    
+        
     playerX += playerX_change
 
     # horizontal boundaries
@@ -88,6 +93,11 @@ while running:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+
+    # collision detection
+
+    if abs(enemyX - bulletX) < 20 and abs(enemyY - bulletY) < 20:
+         print("Hit!")
 
     # enemy moving down when they hit the wall
     enemyX += enemyX_change
